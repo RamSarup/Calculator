8e44adf7e5b70d90330f983688567705e426780e
RamSarup <ramsarup516@gmail.com>
2026-02-13 15:40:45 +0530

    Added substraction

--- src/calculator.java
+++ src/calculator.java
@@ -4,8 +4,8 @@ public class Main {
         int num1 = 10;
         int num2 = 20;
 
-        int sum = num1 + num2;
+        int sub = num1 - num2;
 
-        System.out.println("Sum = " + sum);
+        System.out.println("Difference = " + sub);
     }
 }
